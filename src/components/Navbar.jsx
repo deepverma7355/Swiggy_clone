c479d9b11c631890c23870b411cfe4c10d802fec
Deep Verma <dv994998@gmail.com>
2026-01-25 20:51:28 +0530

    Add routing to Sign In button in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import logo from "../assets/swiggy_logo_white.avif";
-
+import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between px-16 py-4 bg-orange-500">
@@ -9,11 +9,11 @@ const Navbar = () => {
         alt="Swiggy Logo"
         className="h-10 cursor-pointer"
       />
-
+  <Link to="/login">
         <button className="bg-black px-4 py-2 rounded-lg text-white">
           Sign In
         </button>
-      
+      </Link>
     </nav>
   );
 };
