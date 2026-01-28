7b27fd6974975e540ff948163846702c078d72d9
Deep Verma <dv994998@gmail.com>
2026-01-28 20:32:34 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const cors = require("cors")
+require("dotenv").config()
+
+const connectDB = require("./config/db")
+const authRoutes = require("./routes/authRoutes")
+
+const app = express()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// connect DB
+connectDB()
+
+// routes
+app.use("/api/auth", authRoutes)
+
+app.get("/", (req, res) => {
+  res.send("API Running...")
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
