41c310c429f2e0218312abb70917010c3a54fcc3
Deep Verma <dv994998@gmail.com>
2026-01-31 22:54:54 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,17 @@
 import FoodCategories from "../components/FoodCategories";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import ServiceCard from "../components/ServiceCard";
 import DineoutSection from "../components/DineoutSection";
 import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
-      <div className="bg-orange-500 pb-10">
-        <Navbar />
-        <Hero />
-
-        <div className="flex justify-center gap-10 -mt-10">
-          <ServiceCard />
-        </div>
-      </div>
-
+      <Navbar />
+      <Hero />
       <FoodCategories />
-      <DineoutSection/>
-      <Footer/>
+      <DineoutSection />
+      <Footer />
     </>
   );
 };
