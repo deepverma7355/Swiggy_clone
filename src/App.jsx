becb7a49f368aa3d2e3676d257cf65783d6e4148
Deep Verma <dv994998@gmail.com>
2026-02-03 12:48:04 +0530

    Add new routes for Burger, Cake, Roll, and Noodles

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Pizza from "./pages/Pizza"
 import Biryani from "./pages/Biryani"
+import Burger from "./pages/Burger"
+import Cake from "./pages/Cake"
+import Roll from "./pages/Roll"
+import Noodels from "./pages/Noodles"
 function App() {
   return (
     <Routes>
@@ -12,6 +16,10 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/pizza" element={<Pizza />} />
       <Route path="/biryani" element={<Biryani />} />
+      <Route path="/burger" element={<Burger />} />
+      <Route path="/cake" element={<Cake />} />
+      <Route path="/rolls" element={<Roll />} />
+      <Route path="/noodles" element={<Noodels />} />
     </Routes>
   )
 }
