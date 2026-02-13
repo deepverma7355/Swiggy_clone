8f319c67387dac12dc5751ee622f13f990a49347
Deep Verma <dv994998@gmail.com>
2026-02-13 13:16:54 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,43 +1,48 @@
 import card1 from "../assets/card1.avif";
 import card2 from "../assets/card2.avif";
+import { Link } from "react-router-dom"
+
 const Hero = () => {
   return (
-    <section className="bg-orange-500 text-center py-10">
-      <h1 className="text-white text-4xl font-bold mb-6">
+    <section className="bg-orange-500 text-center py-10 px-4">
+      <h1 className="text-white text-3xl md:text-4xl font-bold mb-6">
         Order food. Discover best restaurants. Swiggy it!
       </h1>
 
-      <div className="flex justify-center gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 max-w-3xl mx-auto">
         <input
           type="text"
           placeholder="Enter your delivery location"
-          className="px-4 py-3 rounded-lg w-64"
+          className="px-4 py-3 rounded-lg w-full"
         />
         <input
           type="text"
           placeholder="Search for restaurant, item or more"
-          className="px-4 py-3 rounded-lg w-96"
+          className="px-4 py-3 rounded-lg w-full"
         />
       </div>
-      <div className="flex justify-center gap-8 mt-10">
-        <div className=" rounded-3xl cursor-pointer w-[400px] hover:scale-105 transition">
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-10 max-w-4xl mx-auto">
+        <div className="rounded-3xl cursor-pointer hover:scale-105 transition">
+          <Link to="/food">
           <img
             src={card1}
             alt="Food Delivery"
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover rounded-3xl"
           />
+          </Link>
         </div>
 
-        <div className=" rounded-3xl cursor-pointer w-[400px] hover:scale-105 transition">
-          <img
-            src={card2}
-            alt="Dineout"
-            className="w-full h-full object-cover "
-          />
+        <div className="rounded-3xl cursor-pointer hover:scale-105 transition">
+            <img
+              src={card2}
+              alt="Dineout"
+              className="w-full h-full object-cover rounded-3xl"
+            />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
