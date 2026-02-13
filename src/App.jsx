1316124e8e2a97546d844428e218d2d9a197f823
Deep Verma <dv994998@gmail.com>
2026-02-13 13:18:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
+import FoodOff from "./pages/FoodOff"
 import Pizza from "./pages/Pizza"
 import Biryani from "./pages/Biryani"
 import Burger from "./pages/Burger"
@@ -10,28 +11,29 @@ import Roll from "./pages/Roll"
 import Noodels from "./pages/Noodles"
 import Kebab from "./pages/Kebab"
 import Chai from "./pages/Chai"
-import Poori from "./pages/Poori"
 import Dosa from "./pages/Dosa"
 import Pasta from "./pages/Pasta"
 import Paratha from "./pages/Paratha"
+import Poori from "./pages/Poori"
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/food" element={<FoodOff />} />
       <Route path="/pizza" element={<Pizza />} />
       <Route path="/biryani" element={<Biryani />} />
       <Route path="/burger" element={<Burger />} />
       <Route path="/cake" element={<Cake />} />
       <Route path="/rolls" element={<Roll />} />
       <Route path="/noodles" element={<Noodels />} />
-      <Route path="/kebab" element={<Kebab />} /> 
+      <Route path="/kebab" element={<Kebab />} />
       <Route path="/chai" element={<Chai />} />
-      <Route path="/poori" element={<Poori />} />
       <Route path="/dosa" element={<Dosa />} />
       <Route path="/pasta" element={<Pasta />} />
       <Route path="/paratha" element={<Paratha />} />
+      <Route path="/poori" element={<Poori />} />
     </Routes>
   )
 }
